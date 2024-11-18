8b265afbd403d47c0e47a15a7197e339085cc048
Viktoriia Chykrii <vika.chikrii@gmail.com>
2024-11-18 23:32:13 +0100

    set tests to run in headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000/',
 
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
